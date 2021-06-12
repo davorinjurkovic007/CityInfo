f21e4d1aeded133acd7d8cde0a594d05969cfa14
Davorin <davorin.jurkovic@gmail.com>
2021-06-12 23:08:28 +0200

    Creating the API and Returning Resources

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,14 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc();
+            services.AddControllers()
+                .AddNewtonsoftJson(setupAction =>
+                {
+                    //setupAction.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    //setupAction.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                })
+                .AddXmlDataContractSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +34,32 @@ namespace CityInfo.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                 app.UseExceptionHandler();
+            }
+
+            app.UseStatusCodePages();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //    //throw new Exception("Example exception");
+                //});
             });
+
+            
+
+            //app.Run((context) =>
+            //{
+            //    //await context.Response.WriteAsync("Hello World!");
+            //    throw new Exception("Example exception");
+            //});
         }
     }
 }
